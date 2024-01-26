9f1588017a71e8c8d85908d85647009cc1257a1b
sadiqawan <awansadiq09@gmail.com>
2024-01-26 23:01:49 +0500

    Models of playes and Data of player are added

---
+++ lib/data_store/player_data_store.dart
@@ -0,0 +1,81 @@
+import 'package:pak_cricket_team_app/models/player_model.dart';
+
+List<Player> playre = [
+  Player(
+      name: 'Baber Azam',
+      image: '',
+      shart: 52,
+      role: 'Right hand batsman',
+      history:
+          "Full Name: Babar Azam Born: 15 Oct 1994 Lahore, Punjab, Pakistan Current age: 29 years 102 day(s) Major teams: Pakistan, Pakistan Under-19s, Pakistan A, Lahore Region Under-16s, National Cricket Academy Colts, Lahore North Zone Whites Under-19s, Lahore Eagles, Islamabad Leopards, Federal United, Islamabad United Batting style: Right handed Bowling style: Right Arm Off Break Spin"),
+  Player(
+      name: 'Fakhar Zaman',
+      image: '',
+      shart: 39,
+      role: 'Left hand batsman',
+      history:
+          "Full Name: Fakhar Zaman Born: 10 Apr 1990 Mardan, North-West Frontier Province, Pakistann Current age: 33 years 290 day(s) Major teams: Pakistan A, Pakistan, Pakistan Navy, Mardan, Karachi Dolphins, Abbottabad Falcons, Abbottabad Region, Balochistan, Karachi Whites Under-19s, Karachi Zone VI Batting style: Left handed Bowling style: Left Arm orthodox"),
+  Player(
+      name: 'Iftikhar Ahmad',
+      image: '',
+      shart: 95,
+      role: 'Right Hand Batsman',
+      history:
+          'Full Name: Iftikhar Ahmed Born: 03 Sep 1990 Peshawar, North-West Frontier Province, Pakistan Current age: 33 years 144 day(s) Major teams: Pakistan A, Pakistan, Federally Administered Tribal Areas, Karachi Kings, Peshawar Zalmi, Sindh, PCB Whites, Nowshehra, Khyber Pakhtunkhwa Fighters, Islamabad Batting style: Right handed Bowling style: Right Arm Off Break Spin'),
+  Player(
+      name: 'Haris Rauf',
+      image: '',
+      shart: 97,
+      role: 'Right Hand Fast Bowler',
+      history:
+          'Full Name: Haris Rauf Born: 07 Nov 1993 Rawalpindi Current age: 30 years 79 day(s) Major teams: Pakistan, Lahore Qalandars, Pakistan Shaheens, Balochistan, Islamabad Region, Northern Batting style: Right handed Bowling style: Right Arm Fast Medium'),
+  Player(
+      name: 'Muhammad Rizwan',
+      image: '',
+      shart: 16,
+      role: 'W/Right Hand Batsman',
+      history:
+          'Full Name: Mohammad Rizwan Born: 01 Jun 1992 Peshawar, North-West Frontier Province, Pakistan Current age: 31 years 238 day(s) Major teams: Pakistan Under-19s, Pakistan, Pakistan Emerging Team, Pakistan A, Khyber Pakhtunkhwa Fighters, Peshawar Region, Team White, Peshawar Panthers, Swat, Pakistan Under-23s Batting style: Right handed'),
+  Player(
+      name: 'Shaheen Afridi',
+      image: '',
+      shart: 10,
+      role: 'Left Hand Fast Bowler',
+      history:
+          'Full Name: Shaheen Shah Afridi Born: 06 Apr 2000 Khyber Agency Current age: 23 years 294 day(s) Major teams: Pakistan A, Pakistan Under-19s, Pakistan, FATA Region Under-16s, Khyber Pakhtunkhwa Under-19s, PCB Greens, Lahore Qalandars, Sui Northern Gas Pipelines Limited, Khyber Pakhtunkhwa, FATA Region Under 19s Batting style: Left handed Bowling style: Left Arm Medium Fast'),
+  Player(
+      name: 'Muhammad Haris',
+      image: '',
+      shart: 29,
+      role: 'W/Right Hand Batsman',
+      history:
+          'Full Name: Mohammad Haris Born: 30 Mar 2001 Peshawar Current age: 22 years 301 day(s) Major teams: Pakistan, Pakistan Under-19s, Peshawar Under-19s, Khyber Pakhtunkhwa, Peshawar Zalmi, Peshawar Region Under 19s, Karachi Kings, Peshawar Region, Khyber Pakhtunkhwa Under-19s, Pakistan Shaheens Batting style: Right handed'),
+  Player(
+      name: 'Zaman Khan',
+      image: '',
+      shart: 12,
+      role: 'Right Hand Fast Bowler',
+      history:
+          'Full Name: Zaman Khan Born: 10 Sep 2001 Mirpur Current age: 22 years 137 day(s) Major teams: Pakistan, Pakistan Under-16s, Azad Jammu and Kashmir Region Under 19s, Lahore Qalandars, Azad Jammu and Kashmir Region Under-16s, Mirpur Under-19s, Federal Capital Under-16s, Northern Under-19s, Rawalpindi Region, Northern Batting style: Right handed Bowling style: Right Arm Medium Fast'),
+  Player(
+      name: 'Naseem shah',
+      image: '',
+      shart: 71,
+      role: 'Right Hand Fast Bowler',
+      history:
+          'Full Name: Naseem Shah Born: 15 Feb 2003 Current age: 20 years 345 day(s) Major teams: Punjab Under-19s, Pakistan Under-16s, Pakistan, Pakistan Under-19s, Lahore Region Under-16s, Lahore Region Whites, Team Green, Khyber Pakhtunkhwa, Lahore East Zone Whites Under-19s, Central Punjab Batting style: Right handed Bowling style: Right Arm Fast Medium'),
+  Player(
+      name: 'Saim Ayub',
+      image: '',
+      shart: 63,
+      role: 'Left Hand Batsman',
+      history:
+          'Full Name: Saim Ayub Born: 24 May 2002 Current age: 21 years 246 day(s) Major teams: Pakistan Under-16s, Pakistan Under-19s, Pakistan, Karachi Region Whites, Quetta Gladiators, Karachi Region Under-16s, Karachi Region Whites Under 19s, Karachi Zone VI Under-19s, Peshawar Zalmi, Sindh Under-16s Batting style: Left handed Bowling style: Right Arm Medium Fast'),
+  Player(
+      name: 'Shadab Khan ',
+      image: '',
+      shart: 29,
+      role: 'All-rounder',
+      history:
+          'Full Name: Shadab Khan Born: 04 Oct 1998 Mianwali Current age: 25 years 113 day(s) Major teams: Pakistan Under-19s, Pakistan, Pakistan A, Attock Under-19s, Rawalpindi Region, Khyber Pakhtunkhwa, PCB Greens, Rawalpindi Rams Under-19s, Karachi Electric Supply Corporation Limited, Islamabad United Batting style: Right handed Bowling style: Right Arm Leg Break Spin'),
+];
---
+++ lib/models/player_model.dart
@@ -0,0 +1,14 @@
+class Player {
+  late String name;
+  late String image;
+  late int shart;
+  late String history;
+  late String role;
+
+  Player(
+      {required this.name,
+      required this.image,
+      required this.shart,
+      required this.role,
+      required this.history});
+}
